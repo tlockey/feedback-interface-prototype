e76a8dca894423fce1a58f1e66dcfd68752ee0b3
westonludeke <westonludeke@gmail.com>
2024-03-06 13:38:25 -0600

    install cors to backend and update server.js file. update App.jsx and sever.js to move routing logic to express backend. update fallback URL to example 404 page and add example.com to toToUrlMap

--- feedback-interface/src/App.jsx
+++ feedback-interface/src/App.jsx
@@ -8,14 +8,8 @@ import {
   useParams,
 } from "react-router-dom";
 
-// Sample mapping of IDs to URLs
-const idToUrlMapping = {
-  "1234": "https://zipxam.com",
-  "6661": "https://westonludeke.com",
-  "5678": "https://rainbucket.xyz"
-};
-
-const pullRequestCommentsLink = "https://api.github.com/repos/rainbucket-xyz/rainbucket/issues/17/comments";
+const pullRequestCommentsLink =
+  "https://api.github.com/repos/rainbucket-xyz/rainbucket/issues/17/comments";
 const installationLink = "https://api.github.com/orgs/rainbucket-xyz/installation";
 const jwtLink = "http://localhost:3002/jwt";
 
@@ -30,7 +24,6 @@ async function getComments() {
 
 async function generateToken() {
   const response = await axios.get(jwtLink);
-  console.log(response.data);
   const { jwt } = response.data;
   return jwt;
 }
@@ -41,60 +34,56 @@ async function getInstallationID(jwt) {
     "X-GitHub-Api-Version": "2022-11-28",
     Authorization: `Bearer ${jwt}`,
   };
-
-  // Get installation ID
   let response = await axios.get(installationLink, { headers });
   return response.data.id;
 }
 
 async function getAuthentication() {
-  // Get JWT Token
   const jwt = await generateToken();
-  console.log("generated jwtToken: ", jwt);
-
-  // Get installation id using jwt
   const installationID = await getInstallationID(jwt);
-  console.log("installationID: ", installationID);
-
   const headers = {
     Accept: "application/vnd.github+json",
     "X-GitHub-Api-Version": "2022-11-28",
     Authorization: `Bearer ${jwt}`,
   };
-
-  // Get installation access token
   const installationAccessTokenLink = `https://api.github.com/app/installations/${installationID}/access_tokens`;
   let response = await axios.post(installationAccessTokenLink, null, { headers });
-
-  console.log(response.data);
   return response.data.token;
 }
 
 async function postComment(comment) {
   let authorization = await getAuthentication();
-  console.log("authorization: ", authorization);
   let headers = {
     Accept: "application/vnd.github+json",
     "X-GitHub-Api-Version": "2022-11-28",
     Authorization: `Bearer ${authorization}`,
   };
-  let response = await axios.post(pullRequestCommentsLink, { body: comment }, { headers });
-  // probably update the comments in our preview interface
+  await axios.post(
+    pullRequestCommentsLink,
+    { body: comment },
+    { headers }
+  );
+}
+
+async function getUrlById(id) {
+  try {
+    const response = await axios.get(`http://localhost:3001/api/preview/${id}`);
+    return response.data.url;
+  } catch (error) {
+    console.error("Error fetching URL by ID:", error);
+    return "https://zipxam.com/404"; // example 404 page (i.e. fallback URL if underlying page doesn't load)
+  }
 }
 
 function PreviewEnvironment() {
-  const { id } = useParams(); // Fetching the `id` from the URL
-  const [appLink, setAppLink] = useState("https://rainbucket.xyz/"); // Default URL
+  const { id } = useParams();
+  const [appLink, setAppLink] = useState("");
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
-    const url = idToUrlMapping[id];
-    if (url) {
-      setAppLink(url);
-    }
-
-    getComments().then((comments) => setComments(comments));
+    getUrlById(id).then(setAppLink);
+    getComments().then(setComments);
     getAuthentication();
   }, [id]);
 
@@ -112,7 +101,7 @@ function PreviewEnvironment() {
           <Comment user={user} comment={body} key={idx} />
         ))}
         <form onSubmit={onCreateComment}>
-          <label htmlFor="newComment">New Comment: </label>
+          <label htmlFor="newComment">New Comment:</label>
           <input
             id="newComment"
             type="text"
@@ -122,7 +111,7 @@ function PreviewEnvironment() {
           <button type="submit">Post Comment</button>
         </form>
       </div>
-      <iframe src={appLink} title="Preview" ></iframe>
+      <iframe src={appLink} title="Preview"></iframe>
     </>
   );
 }
@@ -130,9 +119,7 @@ function PreviewEnvironment() {
 function Comment({ user, comment }) {
   return (
     <>
-      <p>
-        {comment} - {user}
-      </p>
+      <p>{comment} - {user}</p>
       <hr />
     </>
   );
--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
 const idToUrlMap = {
   '12jk34': 'https://zipxam.com',
   "36wl45": "https://westonludeke.com",
-  "99ab21": "https://rainbucket.xyz"
+  "99ab21": "https://rainbucket.xyz",
+  "66xx61": "https://example.com"
 };
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/preview/:id', (req, res) => {
   const { id } = req.params;

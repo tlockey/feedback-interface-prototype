cfb4897089d869f59dc8d8a135b9d39aa024bc96
westonludeke <westonludeke@gmail.com>
2024-03-06 11:03:14 -0600

    update App.jsx with URL mapping

--- feedback-interface/src/App.jsx
+++ feedback-interface/src/App.jsx
@@ -1,14 +1,22 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
 
-const appLink = "https://rainbucket.xyz/";
-const pullRequestCommentsLink =
-  "https://api.github.com/repos/rainbucket-xyz/rainbucket/issues/17/comments";
+// Sample mapping of IDs to URLs
+const idToUrlMapping = {
+  "1234": "https://zipxam.com",
+  "6661": "https://westonludeke.com",
+  "5678": "https://rainbucket.xyz"
+};
 
-const installationLink =
-  "https://api.github.com/orgs/rainbucket-xyz/installation";
-
+const pullRequestCommentsLink = "https://api.github.com/repos/rainbucket-xyz/rainbucket/issues/17/comments";
+const installationLink = "https://api.github.com/orgs/rainbucket-xyz/installation";
 const jwtLink = "http://localhost:3002/jwt";
 
 async function getComments() {
@@ -44,7 +52,7 @@ async function getAuthentication() {
   const jwt = await generateToken();
   console.log("generated jwtToken: ", jwt);
 
-  // get installation id using jwt
+  // Get installation id using jwt
   const installationID = await getInstallationID(jwt);
   console.log("installationID: ", installationID);
 
@@ -54,12 +62,9 @@ async function getAuthentication() {
     Authorization: `Bearer ${jwt}`,
   };
 
-  // get installation access token
+  // Get installation access token
   const installationAccessTokenLink = `https://api.github.com/app/installations/${installationID}/access_tokens`;
-
-  let response = await axios.post(installationAccessTokenLink, null, {
-    headers,
-  });
+  let response = await axios.post(installationAccessTokenLink, null, { headers });
 
   console.log(response.data);
   return response.data.token;
@@ -73,22 +78,25 @@ async function postComment(comment) {
     "X-GitHub-Api-Version": "2022-11-28",
     Authorization: `Bearer ${authorization}`,
   };
-  let response = await axios.post(
-    pullRequestCommentsLink,
-    { body: comment },
-    { headers }
-  );
+  let response = await axios.post(pullRequestCommentsLink, { body: comment }, { headers });
   // probably update the comments in our preview interface
 }
 
-function App() {
+function PreviewEnvironment() {
+  const { id } = useParams(); // Fetching the `id` from the URL
+  const [appLink, setAppLink] = useState("https://rainbucket.xyz/"); // Default URL
   const [comments, setComments] = useState([]);
-  const [newComment, setNewComment] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
+    const url = idToUrlMapping[id];
+    if (url) {
+      setAppLink(url);
+    }
+
     getComments().then((comments) => setComments(comments));
     getAuthentication();
-  }, []);
+  }, [id]);
 
   const onCreateComment = async (e) => {
     e.preventDefault();
@@ -100,26 +108,21 @@ function App() {
     <>
       <div id="comments">
         <h1>Comments</h1>
-
         {comments.map(({ body, user }, idx) => (
           <Comment user={user} comment={body} key={idx} />
         ))}
-
         <form onSubmit={onCreateComment}>
-          <label for="newComment">New Comment: </label>
+          <label htmlFor="newComment">New Comment: </label>
           <input
             id="newComment"
             type="text"
             value={newComment}
-            onChange={(e) => {
-              setNewComment(e.target.value);
-            }}
+            onChange={(e) => setNewComment(e.target.value)}
           />
           <button type="submit">Post Comment</button>
         </form>
       </div>
-
-      <iframe src={appLink}></iframe>
+      <iframe src={appLink} title="Preview" ></iframe>
     </>
   );
 }
@@ -128,10 +131,20 @@ function Comment({ user, comment }) {
   return (
     <>
       <p>
-        {comment} -{user}
+        {comment} - {user}
       </p>
       <hr />
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/preview/:id" element={<PreviewEnvironment />} />
+      </Routes>
+    </Router>
   );
 }
 
